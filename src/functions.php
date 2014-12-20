c0c6a9bf3547729c9fad864474dcb6db1ca3fa97
Josh Pollock <jpollock412@gmail.com>
2014-12-19 20:42:24 -0500

    add functions.php

---
+++ src/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Helper functions for this package
+ *
+ * @package holotree\modal
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2014 Josh Pollock
+ */ 
